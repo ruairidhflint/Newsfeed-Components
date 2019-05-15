25c3de18b44bc67344483a7fae35514eadaa2f32
Rory Flint <rory@roryflint.co.uk>
2019-05-15 13:27:34 +0100

    stretch goal, added read removal button

--- Article/Article.js
+++ Article/Article.js
@@ -6,19 +6,30 @@ class Article {
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
+    this.readButton = this.domElement.querySelector('.readButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerText = "expand";
+    this.readButton.innerText = "Read";
 
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle.bind(this));
+    this.readButton.addEventListener('click', this.hideArticle.bind(this));
     
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open')
-
+    this.domElement.classList.toggle('article-open');
+    this.expandButton.style = "display: none"
   }
+
+    
+  hideArticle() {
+      // Using our reference to the domElement, toggle a class to expand or hide the article.
+      this.domElement.classList.toggle('hidden1')
+      
+  
+    }
 }
 
 /* START HERE: 
